85925f008a586b564a42009c82c8922dc9a0b98f
Christine Bradshaw <bradshawcfa@gmail.com>
2021-02-06 07:29:55 -0500

    ffuf

--- scripts/http-scans.sh
+++ scripts/http-scans.sh
@@ -60,13 +60,13 @@ fi
 SCREENSHOTS_DIR="screenshots_p""$TPORT"
 if [ ! -d "$SCREENSHOTS_DIR" ]; then
   mkdir "$SCREENSHOTS_DIR"
-  echo -e "##### Created $SCREENSHOTS_DIR"
+  echo -e "##### Created	: $SCREENSHOTS_DIR"
 fi
 # Directory name for parent wget source code files
 WGET_DIR="wget_sourceCodes_p""$TPORT"
 if [ ! -d "$WGET_DIR" ]; then
   mkdir "$WGET_DIR"
-  echo -e "##### Created $WGET_DIR"
+  echo -e "##### Created	: $WGET_DIR"
 fi
 
 #----- CREATE WEB EXTENSION LIST -----#
@@ -83,18 +83,71 @@ done < "$SECLIST_WEBEXT"
 # file extensions
 
 #----- GENERAL SCANNING -----#
+### Nikto ###
+echo -e "##### Running	: Nikto all plugins"
+nikto -h "$IP" -p "$TPORT" -C all -Plugins @@ALL -Save "nikto_requestResponse_p""$TPORT" -output "$IP""_nikto_cAll_plugins-all_p""$TPORT"".txt"
+
+### Whatweb ###
+echo -e "##### Running	: Whatweb aggression 4 verbose"
+whatweb -a 4 "$IP"":""$TPORT" --log-verbose="$IP""_whatweb_aggression4_verbose_p""$TPORT"".txt" 
+
+### Wapiti ###
+echo -e "##### Running	: Wapiti all modules"
+wapiti -u "http://""$IP"":""$TPORT""/" -m "backup,blindsql,buster,crlf,exec,file,htaccess,methods,nikto,permanentxss,redirect,shellshock,sql,ssrf,xss,xxe" --color -v 2 -f txt -o "$IP""_wapiti_color_verbose2_txt_p""$TPORT"
+
+### Nmap NSE ###
+# TODO http-passwd, http-put
+echo -e "##### Running	: NSE http-auth-finder"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-auth-finder" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-auth-finder_p""$TPORT"
+echo -e "##### Running	: NSE http-auth"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-auth" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-auth_p""$TPORT"
+echo -e "##### Running	: NSE http-backup-finder"
+echo "$PASS" | sudo -S nmap -p"$TPORT" -d --script "http-backup-finder" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-backup-finder_p""$TPORT"
+echo -e "##### Running	: NSE http-config-backup"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-config-backup" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-config-backup_p""$TPORT"
+echo -e "##### Running	: NSE http-cookie-flags"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-cookie-flags" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-cookie-flags_p""$TPORT"
+echo -e "##### Running	: NSE http-default-accounts"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-default-accounts" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-default-accounts_p""$TPORT"
+echo -e "##### Running	: NSE http-fileupload-exploiter"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-fileupload-exploiter" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-fileupload-exploiter_p""$TPORT"
+echo -e "##### Running	: NSE http-form-brute"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-form-brute" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-form-brute_p""$TPORT"
+echo -e "##### Running	: NSE http-rfi-spider"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-rfi-spider" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-rfi-spider_p""$TPORT"
+echo -e "##### Running	: NSE http-server-header"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-server-header" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-server-header_p""$TPORT"
+echo -e "##### Running	: NSE http-sql-injection"
+echo "$PASS" | sudo -S nmap -p"$TPORT" --script "http-sql-injection" "$IP" -oA "$IP""_nmap_tcp_sS_nse_http-sql-injection_p""$TPORT"
 
 #----- BRUTE FORCE DIRS/FILES -----#
 BRUTED="bruted_dirs_p$TPORT"
+
+# Run dirb non-recursive
+#echo -e "##### Running	: dirb non-recursive"
+#dirb http://"$IP"":""$TPORT" -r -o "$IP""_dirb_nonRecursive_p""$TPORT"".txt"
+#echo -e "##### Running: dirb recursive"
+#dirb http://"$IP"":""$TPORT" -o "$IP""_dirb_recursive_p""$TPORT"".txt"
+#cat *dirb*p"$TPORT"".txt" | grep ^+ | sort | uniq | awk -F" " '{ print $2 }' >> "$BRUTED" 
+
+# Run GoBuster
+#while IFS= read -r line; do
+  #echo "##### Running	: GoBuster, wordlist=$line"
+  ##gobuster dir -e -u "http://""$IP"":""$TPORT" -t 100 -x "$WEBX_LST" -w "$line" >> "$IP""_gobuster_p$TPORT.txt"
+  #gobuster dir -e -u "http://""$IP"":""$TPORT" -t 100 -w "$line" >> "$IP""_gobuster_p$TPORT.txt"
+#done < "$WRD_LSTS"
+#cat *gobuster_p$TPORT.txt | grep ^http | sort | uniq | awk -F" " '{ print $1 }' >> "$BRUTED"
 
 ### Ffuf ###
 while IFS= read -r line; do
   echo -e "#### Running	: ffuf"
   nm=$(echo $line | awk -F"/" '{ print $NF }' | awk -F"." '{print $1}')
   ffuf -c -w "$line" -u "http://""$IP"":""$TPORT""/FUZZ" -r -c -v -o "$IP""_ffuf_wl_""$nm""_p""$TPORT"".json"
-  #cat "$IP""_ffuf_wl_""$nm""_p""$TPORT"".json"  | jq '.results[].url?' | sed 's/"//g' | sort | uniq >> "$BRUTED"
-  cat *ffuf*.json | jq '.results[] | [.status,.url] | join (" ")' | sed 's/"//g' | grep "^200" | sort | uniq | awk -F" " '{print $NF}' >> "$BRUTED"
+  cat "*ffuf*_p""$TPORT".json | jq '.results[] | [.status,.url] | join (" ")' | sed 's/"//g' | grep "^200" | sort | uniq | awk -F" " '{print $NF}' >> "$BRUTED"
 done < "$WRD_LSTS"
+
+#----- SCAN FOR EXTENSIONS -----#
+
 
 #----- TAKE SCREENSHOTS -----#
 echo "##### Running	: Taking screenshots..."
